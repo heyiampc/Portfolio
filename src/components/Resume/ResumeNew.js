e4fdb34813b7b4a14b9215194d3581c062cd9ce3
Prince Chaurasia <prncchaurasia@gmail.com>
2022-11-25 18:39:20 +0530

    commit

---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const resumeLink =
+  "https://princechaurasia.com/";
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            // variant="primary"
+            href={"https://princechaurasia.com/"}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
